b7feeb61b877df9c40fda715ecc226274ab4f10e
Adrianpogo <adrianpogo15@gmail.com>
2024-03-18 11:11:04 +0100

    Desafio Recursividad v1

--- Ejercicios/DesafioRecursividad.py
+++ Ejercicios/DesafioRecursividad.py
@@ -1,39 +1,30 @@
-def verListaRecursivaConTipoDatos(lista, nivel, tipo):
-  """
-  Función que imprime una lista de forma recursiva, 
-  con sangrías para visualizar la estructura.
+def visualizar_estructura(estructura, nivel=0, index=1):
+    for elemento in estructura:
+        if isinstance(elemento, list):
+            print("  " * nivel + f"{index}: -> <class 'list'>")
+            visualizar_estructura(elemento, nivel + 1, 1)
+        elif isinstance(elemento, dict):
+            print("  " * nivel + f"{index}: -> <class 'dict'>")
+            for key, value in elemento.items():
+                if isinstance(value, (list, dict, tuple, set)):
+                    print("  " * (nivel + 1) + f"{index}:", end="")
+                    visualizar_estructura([value], nivel + 1, 1)
+                else:
+                    print("  " * (nivel + 1) + f"{index}: {key} --> {value}")
+        elif isinstance(elemento, tuple):
+            print("  " * nivel + f"{index}: -> <class 'tuple'>")
+            for i, item in enumerate(elemento):
+                print("  " * (nivel + 1) + f"{index}: {i}: _> {item}")
+        elif isinstance(elemento, set):
+            print("  " * nivel + f"{index}: -> <class 'set'>")
+            for i, item in enumerate(elemento):
+                print("  " * (nivel + 1) + f"{index}: {i}: => {item}")
+        else:
+            print("  " * nivel + f"{index}: -> {elemento}")
+        index += 1
 
-  Parámetros:
-    lista: La lista que se desea imprimir.
-    nivel: El nivel actual de la recursividad.
-    tipo: El tipo de la lista (list, tuple, dict, set).
-  """
+estructura = [1,2,3,(1,2,3),4,5,6,[1,{"key1":1,"key2":2,"key3":{"Alumno1","Alumno2",True}},{1,2,3}]]
 
-  # Imprimir el tipo de la lista
-  if tipo == list:
-    print(f"{nivel}:->")
-  elif tipo == tuple:
-    print(f"{nivel}:_>")
-  elif tipo == dict:
-    print(f"{nivel}:>")
-  elif tipo == set:
-    print(f"{nivel}:=>")
+visualizar_estructura(estructura)
 
-  # Si la lista está vacía, no hay nada que hacer
-  if not lista:
-    return
 
-  # Si el elemento actual es una lista, 
-  # se llama a la función de forma recursiva para cada elemento
-  if isinstance(lista[0], (list, tuple, dict, set)):
-    for i, elemento in enumerate(lista):
-      verListaRecursivaConTipoDatos(elemento, nivel + 1, type(elemento))
-  else:
-    # Si el elemento actual no es una lista, 
-    # se imprime con la sangría correspondiente
-    for i, elemento in enumerate(lista):
-      print(f"{nivel + 1}:-> {elemento}")
-
-# Ejemplo de uso
-l = [1, 2, 3, (1, 2, 3), 4, 5, 6, [1, {"key1": 1, "key2": 2, "key3": {"Alumno1", "Alumno2", True}}, {1, 2, 3}]]
-verListaRecursivaConTipoDatos(l, 0, type(l))
